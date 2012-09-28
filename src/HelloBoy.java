b02035a0648dd7273d94586c1ef50618766af01f
charmyin <976582265@qq.com>
2012-09-28 23:00:18 +0800

    Hello ,I am in myeclipse!
    It changed in web,same changed in myeclipse!

--- src/HelloBoy.java
+++ src/HelloBoy.java
@@ -3,5 +3,6 @@ public class HelloBoy {
 	public static void main(String[] str){
 		System.out.println("Hello Boy!");
 		System.out.println("Good bye boy!");
+		String hello = "In the old place ,myelipse!";
 	}
 }
